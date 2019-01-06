5e9878361f4415cd370218be97eedf1c31a32504
Dhanashree95 <radhika7002@gmail.com>
2019-01-06 14:54:42 +0530

    Phase1.1

--- CucumberFramework/src/test/java/CucumberFramework/Steps/LoginSteps.java
+++ CucumberFramework/src/test/java/CucumberFramework/Steps/LoginSteps.java
@@ -1,71 +1,65 @@
 package CucumberFramework.Steps;
 
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class LoginSteps {
 
+	WebDriver driver;
+
+	@Before()
+	public void setup() {
+		System.setProperty("webdriver.gecko.driver",
+				"D:\\RJ95\\CucumberFramework\\CucumberFramework\\src\\test\\java\\CucumberFramework\\resources\\geckodriver.exe");
+		this.driver = new FirefoxDriver();
+		this.driver.manage().window().maximize();
+		this.driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+
+	}
+
 	@Given("^User navigates to stackoverflow website$")
 	public void user_navigates_to_stackoverflow_website() throws Throwable {
-	   System.out.println("user_navigates_to_stackoverflow_website");
+		driver.get("https://stackoverflow.com/");
 	}
 
 	@Given("^User clicks on the login button on homepage$")
 	public void user_clicks_on_the_login_button_on_homepage() throws Throwable {
-		System.out.println("user_clicks_on_the_login_button_on_homepage");
+		driver.findElement(By.xpath("//a[contains(text(),'Log In')]")).click();
 	}
 
 	@Given("^User enters valid username$")
 	public void user_enters_valid_username() throws Throwable {
-		System.out.println("user_enters_valid_username");
+		Thread.sleep(3000);
+		driver.findElement(By.xpath(".//*[@id='email']")).sendKeys("abc@gmail.com");
 	}
 
 	@Given("^User enters valid password$")
 	public void user_enters_valid_password() throws Throwable {
-		System.out.println("user_enters_valid_password");
+		driver.findElement(By.xpath(".//*[@id='password']")).sendKeys("888");
 	}
 
 	@When("^User clicks on the login button$")
 	public void user_clicks_on_the_login_button() throws Throwable {
-		System.out.println("user_clicks_on_the_login_button");
+		driver.findElement(By.xpath(".//*[@id='submit-button']")).click();
 	}
 
 	@Then("^User should be taken to the successful login page$")
 	public void user_should_be_taken_to_the_successful_login_page() throws Throwable {
-		System.out.println("user_should_be_taken_to_the_successful_login_page");
+		WebElement askQuestionsButton =driver.findElement(By.xpath("//a[contains(text(),'Ask Question')]"));
+		Thread.sleep(3000);
+		Assert.assertEquals(true, askQuestionsButton.isDisplayed());
 	}
 
-	//System.out.println("2");
-	
-	@Given("^User navigates to stackoverflow website(\\d+)$")
-	public void user_navigates_to_stackoverflow_website(int arg1) throws Throwable {
-		System.out.println("user_navigates_to_stackoverflow_website2");
-	}
-
-	@Given("^User clicks on the login button on homepage(\\d+)$")
-	public void user_clicks_on_the_login_button_on_homepage(int arg1) throws Throwable {
-		System.out.println("user_clicks_on_the_login_button_on_homepage2");
-	}
-
-	@Given("^User enters valid username(\\d+)$")
-	public void user_enters_valid_username(int arg1) throws Throwable {
-		System.out.println("user_enters_valid_username2");
-	}
-
-	@Given("^User enters valid password(\\d+)$")
-	public void user_enters_valid_password(int arg1) throws Throwable {
-		System.out.println("user_enters_valid_password2");
-	}
-
-	@When("^User clicks on the login button(\\d+)$")
-	public void user_clicks_on_the_login_button(int arg1) throws Throwable {
-		System.out.println("user_clicks_on_the_login_button2");
-	}
-
-	@Then("^User should be taken to the successful login page(\\d+)$")
-	public void user_should_be_taken_to_the_successful_login_page(int arg1) throws Throwable {
-		System.out.println("user_should_be_taken_to_the_successful_login_page2");
-	}
 }
+
+	
